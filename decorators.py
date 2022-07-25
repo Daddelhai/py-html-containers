5965a9c621384e88febf9e08d0137c99227b1889
Julian Kruchy <julian.kruchy@tu-dresden.de>
2022-07-25 08:50:33 +0200

    Update

--- decorators.py
+++ decorators.py
@@ -6,13 +6,13 @@ from lib.html.document  import Document
 
 def HTMLDocument(title: str = None, lang: str = "en", charset: str = "utf-8"):
     def decorator(func):
-        def wrapper(request):
+        def wrapper(request, *args, **kwargs):
             document = Document(title=title, lang=lang, charset=charset)
 
             if inspect.iscoroutinefunction(func):
-                asyncio.run(func(request, document.head, document.body))
+                asyncio.run(func(request, document.head, document.body, *args, **kwargs))
             else:
-                func(request, document.head, document.body)
+                func(request, document.head, document.body, *args, **kwargs)
             return HttpResponse(str(document))
         return wrapper
     return decorator
